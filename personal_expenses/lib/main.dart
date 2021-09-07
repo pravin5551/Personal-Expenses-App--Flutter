80ff3ef729e6c39caacbac42bc03ee72dcc521ca
pravin5551 <pravinvpawar5551@gmail.com>
2021-09-07 13:08:51 +0530

    foundation created

---
+++ personal_expenses/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.69,
+      dateTime: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New Book',
+      amount: 85,
+      dateTime: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transaction
+                .map((tx) => Card(
+                      color: Colors.red,
+                      child: Text(tx.title),
+                    ))
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
