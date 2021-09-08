b6e365dd870c4fd0f91fb5cb891133642f3614b4
pravin5551 <pravinvpawar5551@gmail.com>
2021-09-08 18:32:52 +0530

    chart showcase added

---
+++ personal_expenses/lib/widgets/chart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expenses/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].dateTime.day == weekDay.day &&
+            recentTransactions[i].dateTime.month == weekDay.month &&
+            recentTransactions[i].dateTime.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+
+      return {'Day': DateFormat.E().format(weekDay), 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupTransactionValues);
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
