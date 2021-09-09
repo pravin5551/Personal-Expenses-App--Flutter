3f3b5c05ad596f6a7766940089ee442888a602ac
pravin5551 <pravinvpawar5551@gmail.com>
2021-09-09 11:48:16 +0530

    delete functionality added

--- personal_expenses/lib/widgets/new_transaction.dart
+++ personal_expenses/lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -11,22 +12,41 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final entredAmount = double.parse(amountController.text);
-    if (enteredTitle.isEmpty || entredAmount <= 0) {
+  void _submitData() {
+    final enteredTitle = _titleController.text;
+    final entredAmount = double.parse(_amountController.text);
+    if (enteredTitle.isEmpty || entredAmount <= 0 || _selectedDate==null) {
       return;
     }
 
     widget.addTx(
       enteredTitle,
       entredAmount,
+      _selectedDate,
     );
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime.now(),
+    ).then((pickDate) {
+      if(pickDate ==null){
+        return;
+      }
+      setState(() {
+        _selectedDate =pickDate;
+
+      });
+    });
   }
 
   @override
@@ -45,23 +65,47 @@ class _NewTransactionState extends State<NewTransaction> {
                 // onChanged: (lol){
                 //   titleInout =lol;
                 // },
-                controller: titleController,
+                controller: _titleController,
               ),
               TextField(
                 decoration: InputDecoration(labelText: 'Amount'),
                 // onChanged: (lol){
                 //   amountInput =lol;
                 // },
-                controller: amountController,
+                keyboardType: TextInputType.number,
+                controller: _amountController,
+                onSubmitted: (_) => _submitData(),
               ),
-              TextButton(
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('Chosen Date: ${_selectedDate==null ?'No date Chose!'
+                           : DateFormat.yMd().format(_selectedDate)}'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                    TextButton(
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _presentDatePicker,
+                    )
+                  ],
+                ),
+              ),
+              ElevatedButton(
                 style: TextButton.styleFrom(
                   primary: Colors.orange,
                 ),
                 onPressed: () {
                   // addTx(titleController.text,
                   //     double.parse(amountController.text));
-                  submitData();
+                  _submitData();
                 },
                 child: Text('Add Transaction'),
               ),
@@ -73,22 +117,3 @@ class _NewTransactionState extends State<NewTransaction> {
   }
 }
 
-/**
- *  TextField(
-    autofocus: true,
-    decoration: InputDecoration(labelText: 'Title'),
-    // onChanged: (lol){
-    //   titleInout =lol;
-    // },
-    controller: titleController,
-    ),
-    TextField(
-    decoration: InputDecoration(labelText: 'Amount'),
-    // onChanged: (lol){
-    //   amountInput =lol;
-    // },
-    controller: amountController,
-    keyboardType: TextInputType.number,
-    // onSubmitted: (_) => submitData,
-    ),
- */
