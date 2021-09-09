29c99b909511cbcae042c38354dd8df81cb2a8f5
pravin5551 <pravinvpawar5551@gmail.com>
2021-09-09 16:28:34 +0530

    Cupertino used on chart bar

--- personal_expenses/lib/widgets/chart_bar.dart
+++ personal_expenses/lib/widgets/chart_bar.dart
@@ -10,41 +10,43 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spenindingPctOfTotal,
-                child: Container(
+    return LayoutBuilder(builder: (ctx, constraint){
+      return Column(
+        children: <Widget>[
+          Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          SizedBox(
+            height: 4,
+          ),
+          Container(
+            height: 60,
+            width: 10,
+            child: Stack(
+              children: <Widget>[
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: spenindingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
-    );
+          SizedBox(
+            height: 4,
+          ),
+          Text(label),
+        ],
+      );
+    },);
   }
 }
